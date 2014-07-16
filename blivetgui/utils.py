9e92fa0af40e324efa0917d894b02d53cf7ff1e4
Vojtech Trefny <vtrefny@redhat.com>
2014-07-16 10:19:16 +0200

    kickstart bugs

--- blivetgui/utils.py
+++ blivetgui/utils.py
@@ -583,9 +583,9 @@ class BlivetUtils():
 		
 		# when running from Live CD, its filesystem is mounted in '/' thus creating huge mess with kickstart
 		# deleting the mountpoint from devicetree is odd hack but simply ignoring this device doesn't work
-		if "/" in self.storage.mountpoints.keys and self.storage.mountpoints["/"].name == "live-rw":
-			live_rw = self.storage.getDeviceByName("live-rw")
-			live_rw.format.mountpoint = "/"
+		if "/" in self.storage.mountpoints.keys() and self.storage.mountpoints["/"].name == "live-rw":
+			live_rw = self.storage.devicetree.getDeviceByName("live-rw")
+			live_rw.format.mountpoint = ""
 	
 	def luks_decrypt(self, blivet_device, passphrase):
 		""" Decrypt selected luks device
