d11fc3fdbf5d2fe4868cc48047fadd316e0f7d76
Vojtech Trefny <vtrefny@redhat.com>
2015-07-17 13:36:50 +0200

    Add tests to test server-client functions
    Signed-off-by: Vojtech Trefny <vtrefny@redhat.com>

---
+++ tests/blivetgui_tests/client_test.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest.mock import MagicMock
+
+import pickle
+
+from blivetgui.blivetguiproxy.client import BlivetGUIClient, ClientProxyObject
+from blivetgui.blivetguiproxy.proxy_utils import ProxyID, ProxyDataContainer
+
+from blivet.size import Size
+
+class BlivetGUIClientTest(unittest.TestCase):
+
+    def test_convert_answer(self):
+        # string (shouldn't be converted at all)
+        msg = "abcdef"
+        converted_answer = BlivetGUIClient._answer_convertTo_object(MagicMock(), msg)
+        self.assertEqual(msg, converted_answer)
+
+        # blivet.size.Size (is picklable too)
+        msg = Size("8 GiB")
+        converted_answer = BlivetGUIClient._answer_convertTo_object(MagicMock(), msg)
+        self.assertEqual(msg, converted_answer)
+
+        # ProxyID object
+        test_dict = {}
+        msg = ProxyID()
+        converted_answer = BlivetGUIClient._answer_convertTo_object(MagicMock(id_dict=test_dict), msg)
+        self.assertTrue(isinstance(converted_answer, ClientProxyObject))
+        self.assertEqual(converted_answer.proxy_id, msg) # pylint: disable=no-member
+
+    def test_convert_args(self):
+        # 'normal' arguments
+        args = ["abcdef", 1, 1.01, True, None, Size("8 GiB")]
+        converted_args = BlivetGUIClient._args_convertTo_id(MagicMock(), args)
+        self.assertEqual(converted_args, args)
+
+        # ClientProxyObject (id of object should be returned)
+        args = [ClientProxyObject(MagicMock(), ProxyID())]
+        converted_args = BlivetGUIClient._args_convertTo_id(MagicMock(), args)
+        self.assertTrue(isinstance(converted_args[0], ProxyID))
+        self.assertEqual(converted_args, [arg.proxy_id for arg in args])
+
+        # ProxyDataContainer
+        args = [ProxyDataContainer(data1="abcdef", data2=Size("8 GiB"), data3=ClientProxyObject(MagicMock(), ProxyID()))]
+        converted_args = BlivetGUIClient._args_convertTo_id(MagicMock(), args)
+        self.assertEqual(converted_args[0].data1, args[0].data1)
+        self.assertEqual(converted_args[0].data2, args[0].data2)
+        self.assertTrue(isinstance(converted_args[0].data3, ProxyID))
+        self.assertEqual(converted_args[0].data3, args[0].data3.proxy_id)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/blivetgui_tests/server_test.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest.mock import MagicMock
+
+import pickle
+
+from blivetgui.blivetguiproxy.server import BlivetUtilsServer, BlivetProxyObject
+from blivetgui.blivetguiproxy.proxy_utils import ProxyID, ProxyDataContainer
+
+from blivet.size import Size
+
+class BlivetUtilsServerTest(unittest.TestCase):
+
+    def test_pickle_answer(self):
+        # string
+        msg = "abcdef"
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(), msg)
+        self.assertEqual(msg, pickle.loads(pickled_msg))
+
+        # None
+        msg = None
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(), msg)
+        self.assertEqual(msg, pickle.loads(pickled_msg))
+
+        # blivet.size.Size
+        msg = Size("8 GiB")
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(), msg)
+        self.assertEqual(msg, pickle.loads(pickled_msg))
+
+        # list of multiple types
+        msg = ["abcdef", 1, 1.01, True]
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(), msg)
+        self.assertEqual(msg, pickle.loads(pickled_msg))
+
+        # BlivetProxyObject
+        msg = BlivetProxyObject(MagicMock(), ProxyID())
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(), msg)
+        # BlivetProxyObject is not pickled, instead of it we pickle its id (ProxyID object)
+        # we compare the id (int) of this id (ProxyID) with id of unpickled object
+        self.assertEqual(msg.id.id, pickle.loads(pickled_msg).id)
+
+        # unpicklable object
+        test_dict = {}
+
+        msg = MagicMock() # MagicMock is definitely not in picklable_types
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(object_dict=test_dict), msg)
+        unpickled_msg = pickle.loads(pickled_msg)
+        # unpicklable objects are not pickled, instead a BlivetProxyObject is created
+        # and its ProxyID is pickled; test we really have a ProxyID object and test
+        # that original object was placed in the dict with proxied-object
+        self.assertTrue(isinstance(unpickled_msg, ProxyID))
+        self.assertEqual(test_dict[unpickled_msg.id].blivet_object, msg)
+
+        # unpicklable objects in list
+        test_dict = {}
+
+        msg = [MagicMock(), "abcdef"]
+        pickled_msg = BlivetUtilsServer._pickle_answer(MagicMock(object_dict=test_dict), msg)
+        unpickled_msg = pickle.loads(pickled_msg)
+        self.assertTrue(isinstance(unpickled_msg, list))
+        self.assertTrue(isinstance(unpickled_msg[0], ProxyID))
+        self.assertEqual(test_dict[unpickled_msg[0].id].blivet_object, msg[0])
+        self.assertEqual(unpickled_msg[1], msg[1])
+
+    def test_convert_args(self):
+        # 'normal' arguments
+        args = ["abcdef", 1, 1.01, True, None]
+        converted_args = BlivetUtilsServer._args_convertTo_objects(MagicMock(), args)
+        self.assertEqual(converted_args, args)
+
+        # ProxyID arguments
+        test_dict = {}
+
+        arg1 = ProxyID()
+        arg1_obj = MagicMock(blivet_object = MagicMock())
+        test_dict[arg1.id] = arg1_obj
+        arg2 = ProxyID()
+        arg2_obj = MagicMock(blivet_object = MagicMock())
+        test_dict[arg2.id] = arg2_obj
+
+        converted_args = BlivetUtilsServer._args_convertTo_objects(MagicMock(object_dict=test_dict), [arg1, arg2])
+        self.assertEqual(converted_args, [arg1_obj.blivet_object, arg2_obj.blivet_object])
+
+        # ProxyDataContainer as an argument
+        test_dict = {}
+
+        arg3 = ProxyID()
+        arg3_obj = MagicMock(blivet_object = MagicMock())
+        test_dict[arg3.id] = arg3_obj
+
+        args = [ProxyDataContainer(data1 = "abcdef", data2 = 1, data3 = arg3)]
+        converted_args = BlivetUtilsServer._args_convertTo_objects(MagicMock(object_dict = test_dict), args)
+        self.assertEqual(converted_args[0]["data1"], "abcdef")
+        self.assertEqual(converted_args[0]["data2"], 1)
+        self.assertEqual(converted_args[0]["data3"], arg3_obj.blivet_object)
+
+class BlivetProxyObjectTest(unittest.TestCase):
+
+    blivet_object = MagicMock()
+    obj_id = ProxyID()
+    proxy_object = BlivetProxyObject(blivet_object, obj_id)
+
+    def test_getattr(self):
+        self.assertEqual(self.proxy_object.attr, self.blivet_object.attr)
+
+    def test_getitem(self):
+        self.assertEqual(self.proxy_object["key"], self.blivet_object["key"])
+
+    def test_str(self):
+        self.assertEqual(str(self.proxy_object), str(self.blivet_object))
+
+    def test_len(self):
+        self.assertEqual(len(self.proxy_object), len(self.blivet_object))
+
+if __name__ == "__main__":
+    unittest.main()
