08827d6c256a155e39cecd96fd8de25b15ff4cad
Vojtech Trefny <vtrefny@redhat.com>
2015-09-02 11:03:55 +0200

    New version 1.0
    Signed-off-by: Vojtech Trefny <vtrefny@redhat.com>

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ print(data_files)
 setup(
     name='blivet-gui',
     packages=['blivetgui'],
-    version='0.3.6',
+    version='1.0',
     description = 'Tool for data storages configuration',
     author='Vojtech Trefny',
     author_email='vtrefny@redhat.com',
