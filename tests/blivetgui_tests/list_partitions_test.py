3dfc6deb7f082a15992b8fcf140800376f3f1876
Vojtech Trefny <vtrefny@redhat.com>
2015-11-05 11:55:39 +0100

    Do not allow deleting/editting protected devices
    Signed-off-by: Vojtech Trefny <vtrefny@redhat.com>
    (cherry picked from commit 81ef3bc8ad0f6764e7f4dacf4540d905c3077a10)

--- tests/blivetgui_tests/list_partitions_test.py
+++ tests/blivetgui_tests/list_partitions_test.py
@@ -16,32 +16,36 @@ class ListPartitionsTest(unittest.TestCase):
         # do not allow deleting free space and non-leaf devices
         device = MagicMock(type="free space")
         self.assertFalse(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=False)
+        device = MagicMock(type="partition", isleaf=False, protected=False)
         self.assertFalse(self.list_partitions._allow_delete_device(device))
 
         # unformatted leaves can be deleted
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type=None))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type=None), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
 
         # only non-active swap can be deleted
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=True))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=True), protected=False)
         self.assertFalse(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=False))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=False), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
 
         # only unmounted devices can be deleted
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="lvmpv", mountable=False))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="lvmpv", mountable=False), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=False))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=False), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=True))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=True), protected=False)
+        self.assertFalse(self.list_partitions._allow_delete_device(device))
+
+        # protected devices can't be deleteded
+        device = MagicMock(type="partition", isleaf=True, protected=True)
         self.assertFalse(self.list_partitions._allow_delete_device(device))
 
         # in kickstart mode, allow delete all leaves
         self.list_partitions.kickstart_mode=True
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=True))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="swap", status=True), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=True))
+        device = MagicMock(type="partition", isleaf=True, format=MagicMock(type="ext4", mountable=True, status=True), protected=False)
         self.assertTrue(self.list_partitions._allow_delete_device(device))
-        device = MagicMock(type="partition", isleaf=False)
+        device = MagicMock(type="partition", isleaf=False, protected=False)
         self.assertFalse(self.list_partitions._allow_delete_device(device))
