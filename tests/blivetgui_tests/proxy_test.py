b10881b4e40f89335a931b61b02c49d9765238ce
Vojtech Trefny <vtrefny@redhat.com>
2015-11-05 15:38:55 +0100

    Add test for communication.proxy_utils
    Signed-off-by: Vojtech Trefny <vtrefny@redhat.com>

---
+++ tests/blivetgui_tests/proxy_test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from blivetgui.communication.proxy_utils import ProxyID, ProxyDataContainer
+
+
+class ProxyUtilsTest(unittest.TestCase):
+
+    def test_proxy_id(self):
+        # test unique ID
+        id1 = ProxyID()
+        id2 = ProxyID()
+
+        self.assertNotEqual(id1.id, id2.id)
+
+    def test_proxy_data_container(self):
+        container = ProxyDataContainer(a="a", b="b", c="c")
+
+        # __iter__
+        args = []
+
+        for item in container:
+            args.append(item)
+
+        self.assertEqual(sorted(args), ["a", "b", "c"])
+
+        # __getitem__
+        self.assertEqual(container["a"], "a")
+
+        # __setitem__
+        container["a"] = 1
+        self.assertEqual(container["a"], 1)
+
+        # __getattr__
+        self.assertEqual(container.b, "b")
+
+        with self.assertRaises(AttributeError):
+            container.d # pylint: disable=W0104
+
+
+if __name__ == "__main__":
+    unittest.main()
