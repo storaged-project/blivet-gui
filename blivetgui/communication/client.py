e11c6e689f05a225767e84363f41052b07bfc688
Vojtech Trefny <vtrefny@redhat.com>
2016-12-08 12:58:35 +0100

    Stop predenting we need auth token for client-server communication

--- blivetgui/communication/client.py
+++ blivetgui/communication/client.py
@@ -96,10 +96,7 @@ class BlivetGUIClient(object):
 
     id_dict = {}
 
-    def __init__(self, server_socket, secret):
-
-        self.secret = secret
-
+    def __init__(self, server_socket):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.connect(server_socket)
         self.mutex = Lock()
@@ -156,7 +153,7 @@ class BlivetGUIClient(object):
         """ Call a method on server
         """
 
-        pickled_data = pickle.dumps((self.secret, "call", method, self._args_convertTo_id(args)))
+        pickled_data = pickle.dumps(("call", method, self._args_convertTo_id(args)))
 
         with self.mutex:
             self._send(pickled_data)
@@ -174,7 +171,7 @@ class BlivetGUIClient(object):
         """ Get a param of proxy_id object
         """
 
-        pickled_data = pickle.dumps((self.secret, "param", proxy_id, param_name))
+        pickled_data = pickle.dumps(("param", proxy_id, param_name))
 
         with self.mutex:
             self._send(pickled_data)
@@ -186,7 +183,7 @@ class BlivetGUIClient(object):
         """ Call remotely a method on proxy_id object
         """
 
-        pickled_data = pickle.dumps((self.secret, "method", proxy_id, method_name, args))
+        pickled_data = pickle.dumps(("method", proxy_id, method_name, args))
 
         with self.mutex:
             self._send(pickled_data)
@@ -198,7 +195,7 @@ class BlivetGUIClient(object):
         """ Ask for a next member of iterable proxy_id object
         """
 
-        pickled_data = pickle.dumps((self.secret, "next", proxy_id))
+        pickled_data = pickle.dumps(("next", proxy_id))
 
         with self.mutex:
             self._send(pickled_data)
@@ -210,7 +207,7 @@ class BlivetGUIClient(object):
         """ Ask for a member of iterable proxy_id object
         """
 
-        pickled_data = pickle.dumps((self.secret, "key", proxy_id, key))
+        pickled_data = pickle.dumps(("key", proxy_id, key))
 
         with self.mutex:
             self._send(pickled_data)
@@ -222,7 +219,7 @@ class BlivetGUIClient(object):
         """ Send a control command to server
         """
 
-        pickled_data = pickle.dumps((self.secret, command, args))
+        pickled_data = pickle.dumps((command, args))
 
         with self.mutex:
             self._send(pickled_data)
@@ -232,7 +229,7 @@ class BlivetGUIClient(object):
 
     def remote_do_it(self, show_progress_clbk):
 
-        pickled_data = pickle.dumps((self.secret, "call", "blivet_do_it", ()))
+        pickled_data = pickle.dumps(("call", "blivet_do_it", ()))
 
         with self.mutex:
             self._send(pickled_data)
@@ -255,7 +252,7 @@ class BlivetGUIClient(object):
         """ Quit the client
         """
 
-        pickled_data = pickle.dumps((self.secret, "quit",))
+        pickled_data = pickle.dumps(("quit",))
 
         with self.mutex:
             self._send(pickled_data)
