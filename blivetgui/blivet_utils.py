9795f026aa5587c9ab2903b83ae40d5e38182935
Vojtech Trefny <vtrefny@redhat.com>
2025-08-20 14:52:13 +0200

    Fix displaying whole-disk MD arrays in installer mode
    In Anaconda MD arrays created directly on top of disks are handled
    as disks so we should do the same.
    
    Resolves: rhbz#2389105

--- blivetgui/blivet_utils.py
+++ blivetgui/blivet_utils.py
@@ -198,7 +198,7 @@ class BlivetUtils(object):
 
         """
 
-        return [device for device in self.storage.disks if device.type != "mdarray"]
+        return [device for device in self.storage.disks]
 
     def get_group_devices(self):
         """ Return list of LVM2 Volume Group devices
